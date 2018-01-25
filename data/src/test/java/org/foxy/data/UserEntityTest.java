e8afb32e3abcfff1631225580ba5e86cd85d20bd
Rémi Renard <remi@guarana-technologies.com>
2018-01-25 16:49:58 +0100

    update unitTest

--- data/src/test/java/org/foxy/data/UserEntityTest.java
+++ data/src/test/java/org/foxy/data/UserEntityTest.java
@@ -6,9 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Test the user entity
  */
 public class UserEntityTest {
 
